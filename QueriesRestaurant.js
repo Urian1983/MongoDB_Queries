10aa4fd17c222b797affbe8518ccb3a5da7605d3
Urian1983 <urianr@gmail.com>
2025-11-03 13:26:01 +0100

    first commit

---
+++ QueriesRestaurant.js
@@ -0,0 +1,134 @@
+
+    // 1. Escriu una consulta per mostrar tots els documents en la col·lecció Restaurants.
+    db.usuarios.find()
+
+    // 2. Escriu una consulta per mostrar el restaurant_id, name, borough i cuisine per tots els documents en la       
+    //col·lecció Restaurants.
+    db.usuarios.find({},{restaurant_id:1, name:1, borough:1, cuisine:1}
+    
+    // 3. Escriu una consulta per mostrar el restaurant_id, name, borough i cuisine, però exclou el camp _id per tots 
+    //els documents en la col·lecció Restaurants.
+    db.usuarios.find({},{_id:0,restaurant_id:1, name:1, borough:1, cuisine:1})
+
+
+    // 4. Escriu una consulta per mostrar restaurant_id, name, borough i zip code, 
+    //però exclou el camp _id per tots els documents en la col·lecció Restaurants.
+    db.usuarios.find({},{_id:0,restaurant_id:1, name:1, borough:1, cuisine:1})
+
+
+    // 5.Escriu una consulta per mostrar tots els restaurants que estan en el Bronx.
+    db.usuarios.find({},{name:1,city:"bronx"}
+    
+
+    //6. Escriu una consulta per mostrar els primers 5 restaurants que estan en el Bronx.
+    db.usuarios.find({},{name:1, city:"bronx"}).limit(5)
+    
+    //7. Escriu una consulta per mostrar el pròxim 5 restaurants després de saltar els primers 5 del Bronx.
+    db.usuarios.find({},{name:1, city:"bronx"}).skip(5).limit(5
+    
+
+    //8. Escriu una consulta per trobar els restaurants que tenen un score de més de 90.
+    db.usuarios.find({"grades.score":{$gt:90}},{name:1,"grades.score":1})
+
+
+    //9. Escriu una consulta per trobar els restaurants que tenen un score de més de 80 però menys que 100.
+    db.usuarios.find({"grades.score":{$gt:80, $lt:100}},{name:1,"grades.score":1})
+
+    //10. Escriu una consulta per trobar els restaurants que es localitzen en valor de latitud menys de -95.754168.
+    db.usuarios.find({ "address.coord.1": { $lt: -95.754168 } }, { name: 1, "address.coord": 1})
+    
+    //11. Escriu una consulta de MongoDB per a trobar els restaurants que no preparen cap cuisine de 'American' 
+    //i la seva   qualificació és superior a 70 i longitud inferior a -65.754168.
+    db.usuarios.find({"address.coord.0":{$lt:-65.754168},cuisine:{$ne:"American "} }, 
+    {name:1,cuisine:1,"address.coord.0":1})
+
+
+    //12. Escriu una consulta per trobar els restaurants que no preparen cap cuisine de 'American' 
+    //i van aconseguir un marcador més de 70 i localitzat en la longitud menys que -65.754168. 
+    //Nota: Fes aquesta consulta sense utilitzar $and operador.
+
+    db.usuarios.find({"grades.score":{$gt:70},"address.coord.0":{$lt:-65.754168} }, 
+    {name:1,"grades.score":1,"address.coord.0":1})
+
+
+    //13.Escriu una consulta per trobar els restaurants que no preparen cap cuisine de 'American' 
+    //i van obtenir un punt de grau 'A' no pertany a Brooklyn. S'ha de mostrar el document segons 
+    //la cuisine en ordre  descendent.
+    db.usuarios.find({cuisine: { $ne: "American" },"grades.grade": "A",borough: { $ne: "Brooklyn" }},
+    { name: 1, cuisine: 1, borough: 1, "grades.grade": 1, _id: 0 }).sort({ cuisine: -1 })
+
+    //14.Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants 
+    //que contenen 'Wil' com les tres primeres lletres en el seu nom.
+    db.usuarios.find({ name: { $regex: /^Wil/i } },{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 })
+
+
+    //15.Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que 
+    //contenen 'ces' com les últimes tres lletres en el seu nom.
+    db.usuarios.find({ name: { $regex: /ces$/i } },{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 })
+    
+
+    //16. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells 
+    //restaurants que contenen 'Reg' com tres lletres en algun lloc en el seu nom.
+    db.usuarios.find({name: {$regex: /Reg/i}},{restaurant_id:1, name:1,borough:1,cuisine:1})
+
+
+    //17.Escriu una consulta per trobar els restaurants que pertanyen al Bronx i van preparar 
+    //qualsevol plat americà o xinès.
+    db.usuarios.find({borough:"Bronx", cuisine:{$in:["American","Chinese"]}}, {restaurant_id:1, name:1, borough:1, cuisine:1})
+    
+    //18.Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells 
+    //restaurants que pertanyen a Staten Island o Queens o Bronx o Brooklyn.
+    db.usuarios.find({borough:{$in:["Staten Island","Queens","Bronx","Brooklyn"]}}, {restaurant_id:1, name:1, borough:1, cuisine:1})
+    
+    
+    //19.Escriu una consulta per trobar el restaurant_id, name, borough i cuisine 
+    //per a aquells restaurants que no pertanyen a Staten Island o Queens o Bronx o Brooklyn.
+    db.usuarios.find({borough:{$ne:["Staten Island","Queens","Bronx","Brooklyn"]}}, {restaurant_id:1, name:1, borough:1, cuisine:1})
+    
+
+    //20.Escriu una consulta per trobar el restaurant_id, name, borough i cuisine 
+    //per a aquells restaurants que aconsegueixin un marcador que no és més de 10.
+    db.usuarios.find({"grades.score":{$lte:10}},{restaurant_id:1, name:1, borough:1, cuisine:1})
+    
+
+    
+    //21.Escriu una consulta per trobar el restaurant_id, name, borough i cuisine 
+    //per a aquells restaurants que preparen peix excepte 'American' i 'Chinees' 
+    //o el name del restaurant comença amb lletres 'Wil'.
+   db.usuarios.find({$or: [{ cuisine: { $nin: ["American", "Chinese"] } },{ name: { $regex: /^Wil/i } }]},{ restaurant_id: 1, name: 1, cuisine: 1, _id: 0 })
+
+
+    //22. Escriu una consulta per trobar el restaurant_id, name, i grades per a aquells 
+    //restaurants que aconsegueixin un grau "A" i un score 11 en dades d'estudi ISODate "2014-08-11T00:00:00Z".
+    
+    //23.Escriu una consulta per trobar el restaurant_id, name i grades per a aquells restaurants on el 2n element 
+    //de varietat de graus conté un grau de "A" i marcador 9 sobre un ISODate "2014-08-11T00:00:00Z".
+    
+    //24.Escriu una consulta per trobar el restaurant_id, name, adreça i ubicació geogràfica 
+    //per a aquells restaurants on el segon element del array coord conté un valor que és més de 42 i fins a 52.
+    
+
+    //25- Escriu una consulta per organitzar el nom dels restaurants en ordre ascendent juntament amb totes les columnes.
+    
+
+    //26. Escriu una consulta per organitzar el nom dels restaurants en ordre descendent juntament amb totes les columnes.
+    
+
+    //27. Escriu una consulta per organitzar el nom de la cuisine en ordre ascendent 
+    //i pel mateix barri de cuisine. Ordre descendent.
+
+
+    //28. Escriu una consulta per saber totes les direccions que no contenen el carrer.
+
+    //29. Escriu una consulta que seleccionarà tots els documents en la col·lecció de restaurants 
+    //on el valor del camp coord és Double.
+    
+    //30. Escriu una consulta que seleccionarà el restaurant_id, name i grade per a aquells restaurants 
+    //que retornin 0 com a resta després de dividir el marcador per 7.
+
+    //31.Escriu una consulta per trobar el name de restaurant, borough, longitud i altitud i cuisine 
+    //per a aquells restaurants que contenen 'mon' com tres lletres en algun lloc del seu nom.
+    
+    //32.Escriu una consulta per trobar el name de restaurant, borough, longitud i latitud i cuisine 
+    //per a aquells restaurants que contenen 'Mad' com primeres tres lletres del seu nom.
+
